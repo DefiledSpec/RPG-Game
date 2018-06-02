d74577f13d16c123e0366f41ed3606f3d94bbc9e
Jesse Harrah <defiledspec@gmail.com>
2018-06-02 13:34:12 -0500

    added html, other changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,83 +34,154 @@ No characters in the game can heal or recover Health Points.
 
 A winning player must pick their characters wisely by first fighting an enemy with low Counter Attack Power. This will allow them to grind Attack Power and to take on enemies before they lose all of their Health Points. Healing options would mess with this dynamic.
  */
-class npc{
-    constructor(hp, ap, cap, isPlayer) {
+class Npc {
+    constructor(name, hp, ap, cap, isPlayer) {
+        this.name = name;
         this.hp = hp;
         this.ap = ap;
         this.cap = cap;
         this.attStreak = 0;
         this.isPlayer = false;
         this.isDead = false;
+        this.monitor();
+        this.display();
+        
     }
-    attack(Npc, Player) {     
-        this.attStreak ++;
-        Npc.hp -= (this.ap * this.attStreak);
-        if(Npc.hp <= 0) {
+    display() {
+
+    }
+    monitor() {
+        let x = this;
+        console.log('Monitoring...    ' + x.name);
+        setInterval(function() {
+            if(x.isDead) {
+                console.log('hi')
+                
+            }
+        }, 1000);
+    }
+    attack(Npc) { 
+        let didAtt = false;
+        if(Npc.hp <= 0){
+            //remove enemy, hide att button, isenemyselected = false;
             Npc.hp = 0;
             Npc.isDead = true;
+            return console.log(`${Npc.name} is already dead`);
+        }    
+        this.attStreak++;
+        let currentAtt = this.ap * this.attStreak;
+        Npc.hp -= currentAtt;
+        didAtt = true;
+        if(didAtt) {
+            console.log(`${this.name} (You) attacked ${Npc.name} for ${currentAtt}. ${Npc.name} has ${Npc.hp} hp left.`);
+            Npc.counter(this);
         }
-        Npc.counter(Player);
     }
     counter(Player) {
+        let didCounter = false;
+        if(this.hp <= 0) {
+            return console.log(`${this.name} has died.`);
+        }
         Player.hp -= this.cap;
+        didCounter = true;
+
         if(Player.hp <= 0) {
             Player.hp = 0;
             Player.isDead = true;
         }
+        if(didCounter) {
+            console.log(`${this.name} countered ${Player.name} for ${this.cap}. ${Player.name} has ${Player.hp} hp left.`);        
+
+        }
     }
 }
-
+let article1 = $('article')[0];
+let article2 = $('article')[1];
+let currEnemyDiv = $('article')[2];
 
 let npcs = [];
+let p1, p2, p3, p4;
+
+let currEnemy = null;
+let userChar = null;
+
+let isGameRunning = false;
+
+let isCharPicked = false;
+let isEnemyPicked = false;
+
+
+$('.npc').click(function(ev) {
+    
+    if(!isCharPicked){
+        $('#pickChar').addClass('hidden');
+        $('#pickEnemy').removeClass('hidden');
+        article2.append(this);
+        let parsedId = parseInt($(this).data('npcid'));
+        userChar = npcs[parsedId];
+        console.log('Player Chose: ' + userChar.name);
+    
+        isCharPicked = true;
+        isGameRunning = true;
+    }else if(!isEnemyPicked){
+        currEnemyDiv.append(this);
+        isEnemyPicked = true;
+        let parsedId =  parseInt($(this).data('npcid'));
+        currEnemy = npcs[parsedId];
+        console.log('Current enemy is: ' + currEnemy.name);
+        
+    }else {
+        
+    }
+
+    if(isEnemyPicked && isCharPicked) {
+        $('#attackBtn').removeClass('hidden');
+    }
+    
+});
+
+
+
 
 function game() {
-    let p1 = new npc(120, 10, 30);
-    let p2 = new npc(150, 3, 20);
-    let p3 = new npc(150, 3, 20);
-    let p4 = new npc(150, 3, 20);
+    p1 = new Npc('Goblin', 120, 10, 30);
+    p2 = new Npc('Imp', 150, 3, 20);
+    p3 = new Npc('Dragon', 150, 3, 20);
+    p4 = new Npc('Owl', 150, 3, 20);
+
     npcs.push(p1);
     npcs.push(p2);
     npcs.push(p3);
     npcs.push(p4);
 
-
-
-
-    p1.attack(p2, p1);
-    p1.attack(p2, p1);
-    p1.attack(p2, p1);
-    p1.attack(p2, p1);
-    p1.attack(p2, p1);
-    
-    
-
-    
-    console.log(p1.hp);
-    console.log(p1.isDead);
-    
-    console.log(p2.hp);
-    console.log(p2.isDead);
-
 }
+
+$('#attackBtn').click(function(){
+
+    userChar.attack(currEnemy);
+})
 
 document.addEventListener('keydown', function (event) {
     let keyCode = event.keyCode;
-    switch(keyCode){
+    switch(keyCode) {
         case 49:
             console.log(event.key);
+            console.log('Player 1 selected.')
             break;
-        case 50:
+            case 50:
             console.log(event.key);
+            console.log('Player 2 selected.')
             break;
-        case 51:
+            case 51:
             console.log(event.key);
+            console.log('Player 3 selected.')
             break;
-        case 52: 
+            case 52: 
             console.log(event.key);
+            console.log('Player 4 selected.')
             break;
         default: 
-            console.log('wrong input');
+            console.log('Wrong Input');
     }
 });
 
